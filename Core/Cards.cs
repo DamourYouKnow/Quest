4d8d7757b2d86f49097811d44e787afb5933585d
damouryouknow <damourba@gmail.com>
2018-01-17 18:30:40 -0500

    added simple card dealer

---
+++ Core/Cards.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cards {
+    public abstract class Card {
+        protected string name;
+    }
+
+    public abstract class BattleCard : Card {
+        // For cards that use battle points (ally, foe, amour, weapon).
+        protected int battlePoints; // Change type to struct later to support sponsored cards.
+    }
+
+    public class AllyCard : BattleCard {
+
+    }
+
+    public class FoeCard : BattleCard {
+
+    }
+
+    public class WeaponCard : BattleCard {
+
+    }
+
+    public class AmourCard : BattleCard {
+
+    }
+
+    public class RankCard : BattleCard {
+
+    }
+
+    public class EventCard : Card {
+     
+    }
+
+    public class TournamentCard : Card {
+
+    }
+
+    public class TestCard : Card {
+
+    }
+
+    public class Hand {
+        public List<Card> Cards;
+    }
+
+    public class Deck {
+        // Intended to be used seperately for each card type.
+        private Stack<Card> cards;
+
+        public int Count {
+            get { return cards.Count; }
+        }
+
+        public Card Draw() {
+            return cards.Pop();
+        }
+
+        public void Shuffle() {
+            List<Card> shuffleList = new List<Card>(cards);
+            Utils.Random.Shuffle<Card>(shuffleList);
+            cards = new Stack<Card>(shuffleList);
+        }
+    }
+
+    public class CardDealer {
+        private Deck deck;
+
+        public CardDealer(Deck deck) {
+            this.deck = deck;
+        }
+
+        public void Deal(List<Hand> hands, int count) {
+            count = Math.Max(count, deck.Count - count);
+            deck.Shuffle();
+
+            foreach (Hand hand in hands) {
+                for (int i = 1; i <= count; i++) {
+                    hand.Cards.Add(deck.Draw());
+                }
+            }
+        }
+    }
+}
+
---
+++ Core/Utils.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Utils {
+    public class Random {
+        public static void Shuffle<T>(IList<T> list) {
+            System.Random rand = new System.Random();
+            for (int i = 0; i < list.Count; i++) {
+                int next = rand.Next(i, list.Count - 1);
+                T temp = list[next];
+                list[next] = list[i];
+                list[i] = temp;
+            }
+        }
+    }
+}
