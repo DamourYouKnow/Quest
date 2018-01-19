512c762fc62aff7440ab7e781a46bf1ce73e18e2
guclouti <guycloutier@cmail.carleton.ca>
2018-01-19 15:09:50 -0500

    Refactor and comment MouseController.cs

--- Assets/Controller/MouseController.cs
+++ Assets/Controller/MouseController.cs
@@ -2,66 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Interacts with the mouse, it's whereabouts, and selected objects
+/// </summary>
 public class MouseController : MonoBehaviour {
-	Vector3 prevPosition; //Position last frame
 	Vector3 currPosition;
-	GameObject selectedGameObject; //Game Object under mouse
-	Vector3 selectedGameObjectOffset;
 	bool isDragging;
 
-	// Use this for initialization
+	//Game Object under mouse
+	GameObject selectedGameObject;
+	//Mouse location relative to selectedGameObject's origin
+	Vector3 selectedGameObjectOffset;
+
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
 	void Start () {
 		isDragging = false;
 	}
 	
-	// Update is called once per frame
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
 	void Update () {
+		//Get position relative to world
 		currPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-		currPosition.z = 0;
+		//Reset z to ensure z remains consistent in our 2d world
+		currPosition.z = 0; 
 
-		if (selectedGameObject != null) {
-			//Debug.Log (selectedGameObject);
-		}
+		if (isDragging)
+			drag ();
 		else {
-			//Debug.Log (currPosition);
-		}
-		//if not dragging then update this.selectedGameObject.  This prevents the case where the mouse moves ahead of the game
-		//object while dragging, causing it to lose focus on the one its dragging.
-		//Also disallows other mouse actions during dragging
-		if (!isDragging) {
-			//Debug.Log ("Not dragging");
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
-			if (Physics.Raycast (ray, out hit)) {
-				selectedGameObject = hit.transform.gameObject;
-				if (selectedGameObject != null) {
-					selectedGameObjectOffset = selectedGameObject.transform.position-currPosition;
-				}
-			} 
-			else {
-				selectedGameObject = null;
-			}
+			raycastSelectedGameObject ();
+
 			if (Input.GetMouseButtonDown (0)) {
-				Debug.Log ("MouseDown");
-				if (selectedGameObject != null) {
-					isDragging = true;
-				}
+				leftMouseButtonDown ();
 			}
-			if (Input.GetMouseButtonUp (0)) {
+		}
+	}
 
-			}
+	/// <summary>
+	/// Used to move selected object with mouse.
+	/// TODO: Implement Draggable interface to distinguish objects that cannot be dragged, currently drags any Game Object.
+	/// </summary>
+	void drag(){
+		if (Input.GetMouseButton (0)) {
+			Vector3 newPosition = currPosition + selectedGameObjectOffset;
+			selectedGameObject.transform.SetPositionAndRotation (newPosition, Quaternion.identity);
 		}
 		else {
-			if (Input.GetMouseButton (0)) {
-				Vector3 newPosition = currPosition + selectedGameObjectOffset;
-				selectedGameObject.transform.SetPositionAndRotation (newPosition, Quaternion.identity);
-			}
-			else {
-				isDragging = false;
+			isDragging = false;
+		}
+	}
+
+	/// <summary>
+	/// Called if left mouse button was pressed down in last frame.
+	/// </summary>
+	void leftMouseButtonDown(){
+		if (selectedGameObject != null) {
+			isDragging = true;
+		}
+	}
+
+	/// <summary>
+	/// Finds object under mouse by means of raycast.
+	/// Will only find Game Objects with Colliders.
+	/// </summary>
+	void raycastSelectedGameObject(){
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		//if hit found
+		if (Physics.Raycast (ray, out hit)) {
+			selectedGameObject = hit.transform.gameObject;
+			if (selectedGameObject != null) {
+				selectedGameObjectOffset = selectedGameObject.transform.position-currPosition;
 			}
 		}
-
-		prevPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-		currPosition.z = 0;
 	}
 }
