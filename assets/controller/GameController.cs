550c26ecea2f9facf64236621489561f47bd2cd4
wowsokawaii <kelvinchoi8a@gmail.com>
2018-01-23 21:53:22 -0500

    added a board/background, GameController.cs and GameController game object

---
+++ assets/controller/GameController.cs
@@ -0,0 +1,20 @@
+﻿//"connected" to the GameController game object
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Quest.Core {
+	public class GameController : MonoBehaviour {
+
+		GameManager gm;
+		// Use this for initialization
+		void Start () {
+			gm = new GameManager();
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+	}
+}
