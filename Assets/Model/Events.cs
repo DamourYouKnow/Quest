100cfb7ea770288b5060e7ef201e88ef4daf281d
damouryouknow <damourba@gmail.com>
2018-01-27 14:00:54 -0500

    event skeleton code

---
+++ Assets/Model/Events.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Quest.Core.Events {
+    interface IEvent {
+        void RunEvent();
+    }
+
+    public class ChivalrousDeedEvent : IEvent {
+        public void RunEvent() {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CourtCalledEvent : IEvent {
+        public void RunEvent() {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CallToArmsEvent : IEvent {
+        public void RunEvent() {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class RecognitionEvent : IEvent {
+        public void RunEvent() {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PlagueEvent : IEvent {
+        public void RunEvent() {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PoxEvent : IEvent {
+        public void RunEvent() {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ProsperityEvent : IEvent {
+        public void RunEvent() {
+            throw new NotImplementedException();
+        }
+    }
+    
+}
