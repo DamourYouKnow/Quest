905d31bd8eec7dcaacf48498a80473053d22f2f6
damouryouknow <damourba@gmail.com>
2018-02-06 13:36:37 -0500

    added draggable behaviour

---
+++ Assets/Controller/Draggable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Dragable : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler {
+    public void OnBeginDrag(PointerEventData eventData) {
+        throw new NotImplementedException();
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        throw new NotImplementedException();
+    }
+}
