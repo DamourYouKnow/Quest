b4264779c27fb7777dbb4800d43ea5c316a7fa1a
damouryouknow <damourba@gmail.com>
2018-02-08 20:11:50 -0500

    moved ai strategies to new file

---
+++ Assets/Model/AIStrategies.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Quest.Core.Cards;
+
+namespace Quest.Core.Players {
+    class AIStrategies {
+        internal class Strategy2 : PlayerBehaviour {
+            public override List<Card> DiscardAfterWinningTest() {
+                throw new NotImplementedException();
+            }
+
+            public override List<Card> NextBid(TestCard testCard) {
+                throw new NotImplementedException();
+            }
+
+            public override bool ParticipateInQuest(QuestCard questCard) {
+                throw new NotImplementedException();
+            }
+
+            public override bool ParticipateInTournament(TournamentCard tournamentCard) {
+                throw new NotImplementedException();
+            }
+
+            public override bool SponsorQuest(QuestCard questCard) {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
--- Assets/Model/Players.cs
+++ Assets/Model/Players.cs
@@ -308,26 +308,4 @@ namespace Quest.Core.Players {
             throw new NotImplementedException();
         }
     }
-
-    internal class Strategy2 : PlayerBehaviour {
-        public override List<Card> DiscardAfterWinningTest() {
-            throw new NotImplementedException();
-        }
-
-        public override List<Card> NextBid(TestCard testCard) {
-            throw new NotImplementedException();
-        }
-
-        public override bool ParticipateInQuest(QuestCard questCard) {
-            throw new NotImplementedException();
-        }
-
-        public override bool ParticipateInTournament(TournamentCard tournamentCard) {
-            throw new NotImplementedException();
-        }
-
-        public override bool SponsorQuest(QuestCard questCard) {
-            throw new NotImplementedException();
-        }
-    }
 }
