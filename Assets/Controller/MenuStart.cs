bf49eb02824286b4bf5c920f74ef38f62dcf373a
wowsokawaii <kelvinchoi8a@gmail.com>
2018-02-10 15:05:50 -0500

    add player buttons adds players now, new questmatch is now created when 'start game' button is pressed

--- Assets/Controller/MenuStart.cs
+++ Assets/Controller/MenuStart.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Quest.Core.Players;
 
 namespace Quest.Core {
 	public class MenuStart : MonoBehaviour {
-
+		
+		int numHumanPlayers = 0;
+		int numAIPlayers = 0;
+		
 		public void changeMenuScene(string sceneName){
 			Application.LoadLevel(sceneName);
 		}
@@ -15,6 +19,23 @@ namespace Quest.Core {
 		
 		public void StartMatch(){
 			QuestMatch questMatch = new QuestMatch();
+			
+			for(int i = 0; i < numHumanPlayers; i++){
+				Player newPlayer = new Player("test human");
+				questMatch.AddPlayer(newPlayer);
+			}
+			for(int i = 0; i < numAIPlayers; i++){
+				Player newAI = new Player("test AI");
+				questMatch.AddPlayer(newAI);
+			}
+		}
+		
+		public void AddHuman(){
+			numHumanPlayers += 1;
+		}
+		
+		public void AddAI(){
+			numAIPlayers += 1;
 		}
 	}
 }
