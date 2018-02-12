e45da586276c1b892c184728e26f6ff5c0ea3ed4
guclouti <guycloutier@cmail.carleton.ca>
2018-02-12 14:56:42 -0500

    Move testscene.unity to Match.unity and add scrollbar to rank to allow for unlimited players.

---
+++ Assets/OpponentScroll.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpponentScroll : MonoBehaviour {
+	Scrollbar scrollbar;
+	float parentOffset;
+	float opponentSize;
+	// Use this for initialization
+	void Start () {
+		scrollbar = this.gameObject.GetComponent<Scrollbar>();
+		scrollbar.onValueChanged.AddListener (valueChanged);
+		parentOffset = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//assumes first child is opponent list
+		scrollbar.numberOfSteps = scrollbar.transform.parent.GetChild (0).childCount;
+		RectTransform oppTransform = (scrollbar.transform.parent.GetChild (0).GetChild(0) as RectTransform);
+		RectTransform screenSize = (scrollbar.transform.parent.parent as RectTransform);
+		opponentSize = oppTransform.rect.width;
+		Debug.Log ((screenSize.rect.width % opponentSize));
+		opponentSize += (screenSize.rect.width % opponentSize);
+		opponentSize /= 100.0f;
+	}
+
+	void valueChanged(float value){
+		scrollbar.transform.parent.transform.GetChild(0).transform.Translate (parentOffset-(value * opponentSize*scrollbar.numberOfSteps), 0, 0);
+		parentOffset = value * opponentSize*scrollbar.numberOfSteps;
+	}
+}
