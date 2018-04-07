8a12f345831fda92a8857ebb87cad2ad92d2e831
damouryouknow <damourba@gmail.com>
2018-04-07 12:46:29 -0400

    added event handler code to unity client

--- Assets/Controller/WebSockets.cs
+++ Assets/Controller/WebSockets.cs
@@ -1,8 +1,12 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
 using UnityEngine;
+using Newtonsoft.Json.Linq;
 
 public class WebSockets : MonoBehaviour {
     private UnityWebSocket socket;
+    private Dictionary<string, Action<JToken>> eventHandlers;
 
     private void Start() {
         this.socket = new UnityWebSocket("ws://localhost:3004/quest");
@@ -10,6 +14,15 @@ public class WebSockets : MonoBehaviour {
         this.socket.OnOpen += this.OnOpen;
         this.socket.OnMessage += this.OnMessage;
         this.socket.OnError += this.OnError;
+
+        this.eventHandlers = new Dictionary<string, Action<JToken>>();
+        this.InitEventHandlers();
+    }
+
+    private void InitEventHandlers() {
+        this.On("test", (data) => {
+            Debug.Log(data.ToString());
+        });
     }
 
     public void OnClose(UnityWebSocket sender, int code, string reason) {
@@ -24,6 +37,16 @@ public class WebSockets : MonoBehaviour {
     public void OnMessage(UnityWebSocket sender, byte[] data) {
         string message = Encoding.UTF8.GetString(data);
         Debug.Log("Message received: " + message);
+
+        JObject eventObj = JObject.Parse(message);
+        string eventName = (string)eventObj["event"];
+        if (this.eventHandlers.ContainsKey(eventName)) {
+            this.eventHandlers[eventName](eventObj["data"]);
+        }
+    }
+
+    public void On(string eventName, Action<JToken> handler) {
+        this.eventHandlers.Add(eventName, handler);
     }
 
     public void SendMessage(UnityWebSocket socket, string message) {
